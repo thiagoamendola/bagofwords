44b0a0891eac3870339ae2bf08e006319bfb19bf
Ticolol <tico.amendola1@gmail.com>
2017-04-28 08:52:10 -0300

    - Created folders with images for dictionary creation, training and test steps
    	- Dictionary creation flow completed

--- test.cpp
+++ test.cpp
@@ -6,21 +6,49 @@
 
 int main(int argc, char **argv){
 
-    float kwords = 0.1;
+    float nclusters = 20;
 
-    DirectoryManager* dir = loadDirectory("images/", 1);
-    FeatureMatrix* fm = computeFeatureVectors(dir, 64);
-    printf("rows:%d cols:%d \n",fm->nFeaturesVectors,fm->featureVector[0]->size);
-    int nclusters = floor(fm->nFeaturesVectors*kwords);
-    FeatureMatrix* clusters = kMeansClustering(fm, nclusters);
-    printf("rows:%d cols:%d \n",clusters->nFeaturesVectors,clusters->featureVector[0]->size);
+    //Dictionary creation
+    BagOfVisualWords* bag = (BagOfVisualWords *)malloc(sizeof(BagOfVisualWords));
+    bag->directoryManager = loadDirectory("images/dict/", 1);
+    bag->vocabulary = computeFeatureVectors(bag->directoryManager, 64);
+    printf("rows:%d cols:%d \n",bag->vocabulary->nFeaturesVectors,bag->vocabulary->featureVector[0]->size);
+    TrainingKnowledge* tr = kMeansClustering(bag->vocabulary, nclusters);
+    printf("rows:%d cols:%d \n", tr->dictionary->nFeaturesVectors,tr->dictionary->featureVector[0]->size);
+    //Fuse BagOfVisualWords and TrainingKnowledge?
+
+    //Training
+    DirectoryManager* trainingDir = loadDirectory("images/training/", 1);
+    //for each images
+    //computeFeatureVectors
+    //for each descriptor found
+    //cmp with vocabulary
+    //sum each ocurrence to word histogram
+
+    //Test
+    DirectoryManager* testDir = loadDirectory("images/test/", 1);
+    //Do the same steps and compare the image histograms
 
 
 
 
-    destroyFeatureMatrix(&fm);
-    destroyFeatureMatrix(&clusters);
-    destroyDirectoryManager(&dir);
+
+
+
+    for (int i=0; i< tr->nlabels; i++)
+        printf("[%d] -> %d\n", i, tr->labels[i]);
+
+
+
+
+    destroyFeatureMatrix(&bag->vocabulary);
+    destroyDirectoryManager(&bag->directoryManager);
+    free(bag);
+    destroyFeatureMatrix(&tr->dictionary);
+    free(tr->labels);
+    free(tr);
+    destroyDirectoryManager(&trainingDir);
+    destroyDirectoryManager(&testDir);
 
 
 
