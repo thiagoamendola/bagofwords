783f9a9ddbbbac18aacc1792c63a53ed6db1a1ca
Ticolol <tico.amendola1@gmail.com>
2017-05-05 21:46:56 -0300

    Finished initial try on main flow

--- test.cpp
+++ test.cpp
@@ -12,41 +12,65 @@ int main(int argc, char **argv){
     BagOfVisualWords* bag = (BagOfVisualWords *)malloc(sizeof(BagOfVisualWords));
     bag->directoryManager = loadDirectory("images/dict/", 1);
     bag->vocabulary = computeFeatureVectors(bag->directoryManager, 32);
-    printf("rows:%d cols:%d \n",bag->vocabulary->nFeaturesVectors,bag->vocabulary->featureVector[0]->size);
+    //printf("rows:%d cols:%d \n",bag->vocabulary->nFeaturesVectors,bag->vocabulary->featureVector[0]->size);
     VocabularyTraining* vr = kMeansClustering(bag->vocabulary, nclusters * bag->vocabulary->nFeaturesVectors);
     printf("rows:%d cols:%d \n", vr->dictionary->nFeaturesVectors,vr->dictionary->featureVector[0]->size);
 
     //Training
     DirectoryManager* trainingDir = loadDirectory("images/training/", 1);
-    TrainingKnowledge* tk = createTrainingKnowledge((int)trainingDir->nfiles, bag->vocabulary->nFeaturesVectors);
+    TrainingKnowledge* trainK = createTrainingKnowledge((int)trainingDir->nfiles, bag->vocabulary->nFeaturesVectors);
     //for each images
-    for (int img = 0; img < (int)trainingDir->nfiles; ++img) {
-        Image* currentImage = readImage(trainingDir->files[img]->path);
-        tk = trainWithImage(img, currentImage, tk, vr);
-        printf("%s\n",trainingDir->files[img]->path);
-        for(int k=0; k < tk->nvocabulary; k++)
-            printf(" %d",tk->imageHistograms[img][k]);
-        printf("\n");
+    for (int i = 0; i < (int)trainingDir->nfiles; ++i) {
+        Image* currentImage = readImage(trainingDir->files[i]->path);
+        trainK = trainWithImage(i, currentImage, trainK, vr);
         //Get label for given histogram
+        char *filename = strrchr(trainingDir->files[i]->path, '/');
+        trainK->labels[i] = int(filename[4]);
+        //printf("%s\n",trainingDir->files[i]->path);
+        //for(int k=0; k < trainK->nvocabulary; k++)
+        //    printf(" %d",trainK->imageHistograms[i][k]);
+        //printf("\n");
         destroyImage(&currentImage);
     }
 
     //Test
     DirectoryManager* testDir = loadDirectory("images/test/", 1);
-    //Do the same steps and compare the image histograms
+    TrainingKnowledge* testK = createTrainingKnowledge((int)testDir->nfiles, bag->vocabulary->nFeaturesVectors);
+    for (int i = 0; i < (int)testDir->nfiles; ++i) {
+        Image* currentImage = readImage(testDir->files[i]->path);
+        testK = trainWithImage(i, currentImage, testK, vr);
+        destroyImage(&currentImage);
+    }
+    findLabels(trainK, testK);
+
+    //Results
+    int imageSum=0, imagesCorrect=0;
+    for (int i = 0; i < (int)testDir->nfiles; ++i) {
+        char *filename = strrchr(testDir->files[i]->path, '/');
+        int correctLabel = int(filename[4]);
+        if(correctLabel == testK->labels[i]){
+            imagesCorrect++;
+        }else{
+            printf(" %s     =>   %c \n", testDir->files[i]->path, testK->labels[i]);
+        }
+        imageSum++;
+    }
+    printf("Total of test images: %d \n", imageSum);
+    printf("Total of correct guesses: %d \n", imagesCorrect);
+    printf("Acuracy: %f \n", (float)imagesCorrect/(float)imageSum);
 
 
 
-
-
-
-
-
-    for(int i = 0; i < tk->nlabels; i++)
-        free(tk->imageHistograms[i]);
-    free(tk->imageHistograms);
-    free(tk->labels);
-    free(tk);
+    for(int i = 0; i < testK->nlabels; i++)
+        free(testK->imageHistograms[i]);
+    free(testK->imageHistograms);
+    free(testK->labels);
+    free(testK);
+    for(int i = 0; i < trainK->nlabels; i++)
+        free(trainK->imageHistograms[i]);
+    free(trainK->imageHistograms);
+    free(trainK->labels);
+    free(trainK);
     destroyFeatureMatrix(&bag->vocabulary);
     destroyDirectoryManager(&bag->directoryManager);
     free(bag);
