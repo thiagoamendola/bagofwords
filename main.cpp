4550811311add521f7e1c58d0effd557b6c34d84
Ticolol <tico.amendola1@gmail.com>
2017-06-29 23:20:44 -0300

    The project now compiles properly
    	- Updated .gitignore
    	- Corrected CMakeLists.txt to compile properly
    	- Minor changes in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,15 @@
 
 
 int main(int argc, char **argv) {
-
-    printf("AE CARAAAAA\n");
-    /*
-
+    printf("AE hooo\n");
 
     Image* image = readImage("obj8__325.pgm");
     Point2DList* intersections = getSuperpixelIntersections(image);
+    printf("MANOOOOO\n");
 
     printf("%d\n", intersections->size);
 
-    destroyImage(&image);
     destroyPoint2DList(&intersections);
-    */
+    destroyImage(&image);
     return 0;
 }
